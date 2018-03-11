0f6ac52ecf26f218498197c966eec411034fe9b3
shawnsalmon <shanesalmon96@gmail.com>
2018-03-11 02:38:44 -0500

    Add files via upload

---
+++ character in put.py
@@ -0,0 +1,38 @@
+#First Python Project Building a characte input.
+
+name = input("Give Me Your Name:  ")
+
+age = input("enter your age: ")
+age = int(age)
+
+birth = 2018 - age
+loo = birth + 100
+
+birth = int(birth)
+loo = int(loo)
+
+print("Your name is: " + name)
+print("Your age is: "  +str(age))
+print("You will turn a 100 on: "  +str(loo))
+
+
+
+num = input("Enter a number")
+num = int(num)
+count = 0
+
+while (count < num):
+    print("Your name is: " + name)
+    print("Your age is: "  +str(age))
+    print("You will turn a 100 on: "  +str(loo))
+    print("\n\n")
+    count = count + 1
+
+
+print ("Thats its Folks")
+
+    
+
+
+
+ 
